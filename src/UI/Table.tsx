25fcf611ec740bf6c757a3334c0f1924329276ba
mohammad-hgh <88213502+Mohammad-HGH@users.noreply.github.com>
2022-07-16 21:19:40 +0430

    remove table check boxes

--- src/UI/Table.tsx
+++ src/UI/Table.tsx
@@ -1,15 +1,20 @@
 import { styled } from '@mui/material';
-import { DataGrid, GridColumns, GridEventListener, GridValidRowModel } from '@mui/x-data-grid';
+import {
+  DataGrid,
+  GridColumns,
+  GridEventListener,
+  GridValidRowModel
+} from '@mui/x-data-grid';
 import React, { Fragment } from 'react';
 
 interface TableValuesType {
-    Rows: readonly GridValidRowModel[];
-    Columns: GridColumns<GridValidRowModel>;
-    onCellEditCommitFn: GridEventListener<'cellEditCommit'>;
+  Rows: readonly GridValidRowModel[];
+  Columns: GridColumns<GridValidRowModel>;
+  onCellEditCommitFn: GridEventListener<'cellEditCommit'>;
 }
 
 const Tables = (props: TableValuesType) => {
-    const MyDataGrid = styled(DataGrid)`
+  const MyDataGrid = styled(DataGrid)`
     .MuiDataGrid-row:hover {
       background-color: rgb(140 124 240 / 8%);
     }
@@ -20,20 +25,20 @@ const Tables = (props: TableValuesType) => {
       outline: solid transparent 1px !important;
     }
   `;
-    return (
-        <Fragment>
-            <div style={{ height: 400, width: '100%' }}>
-                <MyDataGrid
-                    rows={props.Rows}
-                    columns={props.Columns}
-                    pageSize={5}
-                    rowsPerPageOptions={[5]}
-                    checkboxSelection
-                    onCellEditCommit={props.onCellEditCommitFn}
-                />
-            </div>
-        </Fragment>
-    );
+  return (
+    <Fragment>
+      <div style={{ height: 400, width: '100%' }}>
+        <MyDataGrid
+          rows={props.Rows}
+          columns={props.Columns}
+          pageSize={5}
+          rowsPerPageOptions={[5]}
+          checkboxSelection={false}
+          onCellEditCommit={props.onCellEditCommitFn}
+        />
+      </div>
+    </Fragment>
+  );
 };
 
 export default Tables;
